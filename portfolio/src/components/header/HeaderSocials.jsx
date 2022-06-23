d83fd75243fb3da10c302a50cf7d3bae94319f1f
Anubha13kumari <anubhamuz@gmail.com>
2022-06-23 18:15:19 +0530

    removed warning

--- portfolio/src/components/header/HeaderSocials.jsx
+++ portfolio/src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {GrInstagram} from 'react-icons/gr'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://github.com/Anubha13kumari" target="_blank"><FaGithub/></a>
-        <a href="https://www.linkedin.com/in/anubha-kumari-329753221/" target="_blank"><BsLinkedin/></a>
-        <a href="https://www.instagram.com/solely_anubha/" target="_blank"><GrInstagram/></a>
+        <a href="https://github.com/Anubha13kumari" target="_blank" rel="noreferrer"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/anubha-kumari-329753221/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://www.instagram.com/solely_anubha/" target="_blank" rel="noreferrer"><GrInstagram/></a>
     </div>
   )
 }
